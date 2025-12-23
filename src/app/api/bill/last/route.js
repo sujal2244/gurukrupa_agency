80886a4dcfff80caafce721011a5d4c6ca78b7e3
atulj4723 <atulj9537@gmail.com>
2025-12-23 22:44:05 +0530

    Fix sorting order in GET request for last bill; change from ascending to descending by invoice number

--- src/app/api/bill/last/route.js
+++ src/app/api/bill/last/route.js
@@ -2,18 +2,22 @@ import dbConnect from "@/lib/db";
 import Bill from "@/models/Bill";
 
 export const GET = async () => {
-  try {
-    await dbConnect();
-    const res = await Bill.find().sort({ invoiceNumber: 1 }).limit(1);
-    return Response.json(
-      { message: "Data retrived successcfully", success: true, data: res },
-      { status: 200 }
-    );
-  } catch (e) {
-    console.log(e);
-    return Response.json(
-      { message: "Internal Server Error", success: false },
-      { status: 500 }
-    );
-  }
+    try {
+        await dbConnect();
+        const res = await Bill.find().sort({ invoiceNumber: -1 }).limit(1);
+        return Response.json(
+            {
+                message: "Data retrived successcfully",
+                success: true,
+                data: res,
+            },
+            { status: 200 }
+        );
+    } catch (e) {
+        console.log(e);
+        return Response.json(
+            { message: "Internal Server Error", success: false },
+            { status: 500 }
+        );
+    }
 };
