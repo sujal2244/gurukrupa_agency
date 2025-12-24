b2d7d4ccd83b93bf22443eb6ab1385458b6d0643
atulj4723 <atulj9537@gmail.com>
2025-12-24 17:10:28 +0530

    Retrieve the next invoice number instead of the last bill in GET request

--- src/app/api/bill/last/route.js
+++ src/app/api/bill/last/route.js
@@ -4,12 +4,13 @@ import Bill from "@/models/Bill";
 export const GET = async () => {
     try {
         await dbConnect();
-        const res = await Bill.find().sort({ invoiceNumber: -1 }).limit(1);
+        const lastBill = await Bill.findOne().sort({ invoiceNumber: -1 });
+        const nextNumber = lastBill ? lastBill.invoiceNumber + 1 : 1;
         return Response.json(
             {
                 message: "Data retrived successcfully",
                 success: true,
-                data: res,
+                data: nextNumber,
             },
             { status: 200 }
         );
